f62d6e0e2bdc7536f6e2d8cad99d9939324dd30b
youngjin0305 <jiyeon@DESKTOP-ECO4HG5>
2025-09-05 11:15:14 +0900

    add encrypt and decrypt(with CRT) implementation

--- RSA3072/rsa.c
+++ RSA3072/rsa.c
@@ -7,15 +7,32 @@
  * pub_key: 공개키
  */
 void rsa_encrypt(Bignum* ciphertext, const Bignum* message, const RSA_PublicKey* pub_key) {
-
+	bignum_mod_exp(ciphertext, message, &pub_key->e, &pub_key->n);
 }
 
-/**
- * RSA 복호화
- * message: 복호화된 결과를 저장할 Bignum 포인터
- * ciphertext: 복호화할 암호문 Bignum
- * priv_key: 개인키
- */
 void rsa_decrypt(Bignum* message, const Bignum* ciphertext, const RSA_PrivateKey* priv_key) {
+	Bignum m_p, m_q, h;
+
+	// m_p = c^dP mod p
+	bignum_mod_exp(&m_p, ciphertext, &priv_key->dP, &priv_key->p);
+	// m_q = c^dQ mod q
+	bignum_mod_exp(&m_p, ciphertext, &priv_key->dQ, &priv_key->q);
+
+	// h = q^(-1) * (m_p - m_q) mod p
+	if (bignum_compare(&m_p, &m_q) < 0) {
+		bignum_add(&h, &m_p, &priv_key->p);
+		bignum_subtract(&h, &h, &m_q);
+	}
+	else {
+		bignum_subtract(&h, &m_p, &m_q);
+	}
+
+	Bignum temp;
+	bignum_multiply(&temp, &h, &temp, &priv_key->p);
+	bignum_divide(NULL, &h, &temp, &priv_key->p);
+
+	bignum_multiply(&temp, &h, &priv_key->q);
+	bignum_add(message, &m_q, &temp);
+
 
 }
