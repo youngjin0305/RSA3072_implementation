f3f2a770cc4897e761a32930995623d3d1d7bd5b
김영진 <126306228+youngjin0305@users.noreply.github.com>
2025-09-09 17:36:37 +0900

    crt 오류 수정

--- RSA3072/rsa.c
+++ RSA3072/rsa.c
@@ -22,13 +22,10 @@ void rsa_decrypt(Bignum* message, const Bignum* ciphertext, const RSA_PrivateKey
 	else {
 		bignum_subtract(&h, &m_p, &m_q);
 	}
+	bignum_multiply(&h, &h, &priv_key->qInv);
 
-	Bignum temp;
-	bignum_multiply(&temp, &h, &temp, &priv_key->p);
-	bignum_divide(NULL, &h, &temp, &priv_key->p);
-
+    // m = m_q + q * h
+    Bignum temp;
 	bignum_multiply(&temp, &h, &priv_key->q);
 	bignum_add(message, &m_q, &temp);
-
-
 }
